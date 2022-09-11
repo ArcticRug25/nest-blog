3830c8593bb09eab83e2187324a677439ebdd689
ArcticRug25 <wyw82839148@163.com>
2022-09-11 23:52:14 +0800

    chore: add dotenv

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
-
+import { config } from 'dotenv'
+import path from 'path'
+config({
+  path: path.join(__dirname, '../.env'),
+})
+console.log(process.env.NODE_ENV)
 @Module({
   imports: [],
   controllers: [AppController],
