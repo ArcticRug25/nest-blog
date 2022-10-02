ccfaec4cae0048a59e4bd31d69949c3c10e7057b
ArcticRug25 <wyw82839148@163.com>
2022-10-02 12:05:35 +0800

    feat: dto

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,13 +6,14 @@ import { AppService } from './app.service'
 import { config } from 'dotenv'
 import { UploadModule } from './upload/upload.module'
 import { PModule } from './p/p.module';
+import { LoginModule } from './login/login.module';
 import path from 'path'
 config({
   path: path.join(__dirname, '../.env'),
 })
 console.log(process.env.NODE_ENV)
 @Module({
-  imports: [UploadModule, PModule],
+  imports: [UploadModule, PModule, LoginModule],
   controllers: [AppController],
   providers: [
     AppService,
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { NestFactory } from '@nestjs/core'
 import { NestExpressApplication } from '@nestjs/platform-express'
 import { AppModule } from './app.module'
 import { Response } from './common/response'
+import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule)
@@ -15,6 +16,8 @@ async function bootstrap() {
   app.useGlobalInterceptors(new Response())
 
   app.useGlobalFilters(new HttpFilter())
+
+  app.useGlobalPipes(new ValidationPipe())
 
   await app.listen(3000)
 }
