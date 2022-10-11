b5cf2128a5b199260c974c97969e29484fd27d2f
ArcticRug25 <wyw82839148@163.com>
2022-10-12 07:07:06 +0800

    feat: add validate

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,32 @@
-import { ConfigService } from '@nestjs/config'
-import * as common from '@nestjs/common'
+import { HdPipe } from './hd.pipe'
+import { PrismaClient } from '@prisma/client'
+import { Get, Controller, Param, ParseIntPipe, Post, Body } from '@nestjs/common'
 import { AppService } from './app.service'
+import CreateArticleDto from './dto/create.article.dto'
 
-@common.Controller()
+@Controller()
 export class AppController {
-  constructor(private readonly appService: AppService, private readonly configService: ConfigService) {}
-
-  @common.Get()
-  getHello(): string {
-    return this.configService.get('APP')
-    // return this.appService.getHello()
+  prisma: PrismaClient
+  constructor(private readonly appService: AppService) {
+    this.prisma = new PrismaClient()
   }
+
+  @Post('store')
+  async add(@Body(HdPipe) dto: CreateArticleDto) {
+    const article = await this.prisma.article.create({
+      data: {
+        ...dto,
+      },
+    })
+    return dto
+  }
+
+  // @Get(':id')
+  // async getHello(@Param('id', ParseIntPipe) id: number) {
+  //   const a = await this.prisma.article.findUnique({
+  //     where: { id },
+  //   })
+
+  //   return a
+  // }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
+import { ValidateExceptionFilter } from './validate-exception.filter'
+import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { NestExpressApplication } from '@nestjs/platform-express'
 import { AppModule } from './app.module'
+import { Validate } from './validate'
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule)
-
+  app.useGlobalPipes(new Validate())
+  app.useGlobalFilters(new ValidateExceptionFilter())
   await app.listen(3000)
 }
 bootstrap()
